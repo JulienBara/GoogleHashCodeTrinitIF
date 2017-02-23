7fea802759aff0ba4460f6ae36147f54a537d97e
HugoDelval <hugodelval@gmail.com>
2017-02-23 19:11:12 +0000

    printer

--- printer.py
+++ printer.py
@@ -5,9 +5,7 @@ def printer(caches, input_filename):
     f = open('out_' + input_filename + "_" + uuid.uuid4() + '.txt', 'w')
     length = len(caches)
     f.write(str(length) + '\n')
-    for cache in caches:
-        for videos in cache:
-            s = str(videos)
-            f.write(s + ' ')
+    for i, cache in enumerate(caches):
+        f.write(str(i) + " ".join(cache))
         f.write('\n')
     f.close()
