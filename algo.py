0a9a73ddb9edd75efd793cea3926dbbcf75ce865
Wiinterfell <slegras.contact@gmail.com>
2017-02-23 18:32:59 +0000

    dumb solution

--- algo.py
+++ algo.py
@@ -5,6 +5,25 @@ def algo():
     a = parser()
     print(a)
 
+def dumb_solution():
+	nb_caches = 0
+	cache_size = 0
+	videos = {}
+	solution = []
+
+	for cache_id in range(0, nb_caches):
+		left = cache_size
+		solution.add([])
+		while (left > 0):
+			for video_id, video_size in videos:
+				if video_size <= left:
+					solution[cache_id].add(video_id)
+					left = left - video_size
+	return solution
+
+
+
 
 if __name__ == "__main__":
-    algo()
+	solution = dumb_solution()
+	print(solution)
