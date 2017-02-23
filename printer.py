5767ab9b83ea043c456914ba3114c7431c18ce94
HugoDelval <hugodelval@gmail.com>
2017-02-23 19:02:26 +0000

    output printer

--- printer.py
+++ printer.py
@@ -1,14 +1,13 @@
-def printer(caches):
-	f = open('out.txt', 'w')
-	length = len(caches)
-	f.write(str(length)+'\n')
-	for cache in caches:
-		for videos in cache:
-			s = str(videos)
-			f.write(s+' ')
-		f.write('\n')
-	f.close()
+import uuid
 
 
-if __name__ == "__main__":
-    printer()
+def printer(caches, input_filename):
+    f = open('out_' + input_filename + "_" + uuid.uuid4() + '.txt', 'w')
+    length = len(caches)
+    f.write(str(length) + '\n')
+    for cache in caches:
+        for videos in cache:
+            s = str(videos)
+            f.write(s + ' ')
+        f.write('\n')
+    f.close()
