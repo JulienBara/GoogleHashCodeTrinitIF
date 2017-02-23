b8a478b4571ac32b50eed6e411d340c55a56701d
HugoDelval <hugodelval@gmail.com>
2017-02-23 19:39:07 +0000

    all_requests

--- algo.py
+++ algo.py
@@ -3,13 +3,9 @@ from printer import printer
 
 filename = "videos_worth_spreading.in"
 
-def algo():
-    a = parser()
-    print(a)
 
 def dumb_solution():
-    endpoints, videos, nb_caches, cache_size = parser(filename)
-
+    endpoints, videos, nb_caches, cache_size, _ = parser(filename)
     solution = []
 
     for cache_id in range(0, nb_caches):
@@ -19,14 +15,11 @@ def dumb_solution():
         for video_id, video_size in videos.items():
             if int(video_size) <= left:
                 solution[cache_id].append(video_id)
-                left = left - int(video_size)
+                left -= int(video_size)
     return solution
-
-
 
 
 if __name__ == "__main__":
     solution = dumb_solution()
-    #print(solution)
+    # print(solution)
     printer(solution, filename)
-
