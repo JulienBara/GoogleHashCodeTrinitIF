a1642e0e38ac82b65e6cd89b2326c4e2ad9d2953
Wiinterfell <slegras.contact@gmail.com>
2017-02-23 18:40:56 +0000

    first corrections building

--- algo.py
+++ algo.py
@@ -6,20 +6,17 @@ def algo():
     print(a)
 
 def dumb_solution():
-    endpoints, videos, number_of_caches, caches_capacity = parser()
+    endpoints, videos, nb_caches, cache_size = parser("kittens.in")
 
-    nb_caches = 0
-    cache_size = 0
-    videos = {}
     solution = []
 
     for cache_id in range(0, nb_caches):
         left = cache_size
-        solution.add([])
+        solution.append([])
         while (left > 0):
             for video_id, video_size in videos:
                 if video_size <= left:
-                    solution[cache_id].add(video_id)
+                    solution[cache_id].append(video_id)
                     left = left - video_size
     return solution
 
